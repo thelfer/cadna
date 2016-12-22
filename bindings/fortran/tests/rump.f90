426bd72f1e7a71778da00f71da70fb253b0a6472
Helfer Thomas <thomas.helfer@cea.fr>
2016-12-22 10:13:52 +0100

    Fix rump test in fortran bindings

--- bindings/fortran/tests/rump.f90
+++ bindings/fortran/tests/rump.f90
@@ -13,10 +13,13 @@ program main
   type(double_st) y
   type(double_st) z
   type(double_st),external :: rump
-  x = double_st(10864.)
-  y = double_st(18817.)
+  x = double_st(10864._c_double)
+  y = double_st(18817._c_double)
   z=rump(x,y)
-  if(numericalnoise(rump(x,y))==0) then
+  if (1) then
+     write(*,*) 'true'
+  endif
+  if(numericalnoise(rump(x,y))==1) then
      write(*,*) 'numerical noise'
   endif
 end program main
